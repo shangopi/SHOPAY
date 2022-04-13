efd6ce639603f3a1f2f0ba16d9413738f30a8aef
ashok <ashokkumar@hyperslab.com>
2022-04-13 22:27:50 +0530

    simple product list added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -12,7 +12,8 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Hello</h1>
+         
+          <HomeScreen/>
         </Container>
       </main>
       <Footer />
