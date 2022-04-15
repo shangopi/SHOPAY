615419c926a96f50440572e3b2a19345428d8d37
ashok <ashokkumar@hyperslab.com>
2022-04-15 11:30:55 +0530

    add cart remove cart implemented using redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 
 import products from '../products'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
   const product = products.find((p) => p._id === match.params.id)
+  const [qty, setQty] = useState(0)
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
 
   return (
     <>
@@ -15,17 +20,32 @@ const ProductScreen = ({ match }) => {
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
+          
+          <p className='my-5'>Description: {product.description}</p>
         </Col>
         <Col md={3}>
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
+
             <ListGroup.Item>
-             
+            Varient :3GB
             </ListGroup.Item>
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+
+            <ListGroup.Item>
+             SKU-3373G
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+             Weight :300g
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+             Custom attribute :xxxx
+            </ListGroup.Item>
+            <ListGroup.Item className='my-5 bg-info variant-dark'><h2>Price: ${product.price}</h2></ListGroup.Item>
+            
           </ListGroup>
         </Col>
         <Col md={3}>
@@ -48,8 +68,31 @@ const ProductScreen = ({ match }) => {
                   </Col>
                 </Row>
               </ListGroup.Item>
+
+              {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
               <ListGroup.Item>
                 <Button
+                onClick={addToCartHandler}
                   className='btn-block'
                   type='button'
                   disabled={product.countInStock === 0}
