3c324ae0248268cf51e583dcb9a5bfdc2cd1a030
KesaviAravinthan <kesaviap@gmail.com>
2022-05-01 01:30:06 +0530

    modified for admin page view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,12 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import Axios from 'axios'
 import login from './screens/LoginScreen';
+import AddProduct from './admin/addProduct';
+import ViewProduct from './admin/viewProduct';
+import ViewCatecory from './admin/viewCatecory';
+import AddCatecory from './admin/addCategory';
+import AdminHome from './admin/adminHome';
+import Navbar from './admin/navbar';
 const App = () => {
   return (
     <Router>
@@ -19,6 +25,13 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/login' component={login} />
         </Container>
+
+        <Route path='/admin/dashboard' component={AdminHome} exact />
+          <Route path='/admin/addproduct' component={AddProduct} exact />
+          <Route path='/admin/viewproduct' component={ViewProduct} exact />
+          <Route path='/admin/others' component={Navbar} exact />
+          <Route path='/admin/addcategory' component={AddCatecory} exact />
+          <Route path='/admin/viewcategory' component={ViewCatecory} exact />
       </main>
       <Footer />
     </Router>
